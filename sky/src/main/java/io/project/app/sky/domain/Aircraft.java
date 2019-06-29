5117b0a36873587dcea6dfa0b5b1ed70fae0ae14
armena <armena@YLT160003.corp.optym.net>
2019-06-29 15:14:19 +0400

    update

--- sky/src/main/java/io/project/app/sky/domain/Aircraft.java
+++ sky/src/main/java/io/project/app/sky/domain/Aircraft.java
@@ -1,18 +1,29 @@
 package io.project.app.sky.domain;
 
-import java.io.Serializable;
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-@AllArgsConstructor
-@Document(collection = "aircraft")
-public class Aircraft implements Serializable{
-
+@Document(collection="aircraft")
+@NoArgsConstructor
+public class Aircraft {
     @Id
     private String icao;
     private String registration, manufacturericao, manufacturername, model, owner, operator, reguntil, engines, built;
+
+    public Aircraft(String icao, String registration, String manufacturericao, String manufacturername, String model, String owner, String operator, String reguntil, String engines, String built) {
+        this.icao = icao;
+        this.registration = registration;
+        this.manufacturericao = manufacturericao;
+        this.manufacturername = manufacturername;
+        this.model = model;
+        this.owner = owner;
+        this.operator = operator;
+        this.reguntil = reguntil;
+        this.engines = engines;
+        this.built = built;
+    }
 
 }
